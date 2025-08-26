b61d8a73df9ec28a35cd98d2036f8063bef281c9
OllieBurke <ollie0burke@gmail.com>
2025-08-26 13:01:52 +0000

    adding EMRI params from FEW paper

--- EMRI_Params.py
+++ EMRI_Params.py
@@ -14,61 +14,67 @@ except ImportError:
 # ================== CASE 1 IN TABLE ======================
 # EMRI source, eps ~ 1e-5, SNR 50, prograde
 
-# M = 1e6; mu = 10; a = 0.998; p0 = 7.7275; e0 = 0.73; x_I0 = 1.0
+# m1 = 1e6; m2 = 10; a = 0.998; p0 = 7.7275; e0 = 0.73; xI0 = 1.0
 # SNR_choice = 50.0;
+# dist = 2.20360838037185 # Animal -- hardcoded distance for case 1
 # qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
 # Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
-# delta_t = 5.0; T = 2.0
+# dt = 5.0; T = 2.0
 
 # ==================== PLUNGING EMRI =====================
-M = 1e6; mu = 100; a = 0.99; p0 = 14.7275; e0 = 0.4; x_I0 = 1.0
-SNR_choice = 30.0
-qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
-Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
-delta_t = 5.0; T = 2.0
+# m1 = 1e6; m2 = 100; a = 0.99; p0 = 14.7275; e0 = 0.4; xI0 = 1.0
+# SNR_choice = 30.0
+# dist = 2.20360838037185 # Animal -- hardcoded distance for case 1
+# qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
+# Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
+# dt = 5.0; T = 2.0
 
 # ================== CASE 2 IN TABLE ======================
 # Extreme EMRI, mass-ratio 1e-6 
-# M = 1e7; mu = 1e1; a = 0.998; p0 = 2.12; e0 = 0.425; x_I0 = 1.0
+# m1 = 1e7; m2 = 1e1; a = 0.998; p0 = 2.12; e0 = 0.425; xI0 = 1.0
 # SNR_choice = 30;
+# dist = 3.590;
 # qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
 # Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
-# delta_t = 10.0; T = 2.0;
+# dt = 10.0; T = 2.0;
 
 # ================== CASE 3 IN TABLE ======================
 # MEGA IMRI -- Pints. TDI2, SNR = 
 
-# M = 1e7; mu = 100_000; a = 0.95; p0 = 23.4250; e0 = 0.85; x_I0 = 1.0
-# SNR_choice = 500.0; 
-# qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
-# Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
-# delta_t = 5.0; T = 2.0;
+m1 = 1e7; m2 = 100_000; a = 0.95; p0 = 23.4250; e0 = 0.85; xI0 = 1.0
+SNR_choice = 500.0; 
+dist = 3.797946825087277;
+qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
+Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
+dt = 5.0; T = 2.0;
 
 
 
 # ================== CASE 4 IN TABLE ======================
 # MEGA IMRI -- Pints. TDI2, SNR = 
-# M = 1e5; mu = 1e3; a = 0.95; p0 = 74.38304; e0 = 0.85; x_I0 = 1.0
+# m1 = 1e5; m2 = 1e3; a = 0.95; p0 = 74.38304; e0 = 0.85; xI0 = 1.0
 # SNR_choice = 200; 
+# dist = 3.500;
 # qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
 # Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
 
-# delta_t = 2.0; T = 2.0;
+# dt = 2.0; T = 2.0;
 
 
 # ================== CASE 5 IN TABLE ======================
 # EMRI source, eps ~ 1e-4, SNR 30, retrograde
 
-# M = 1e5; mu = 10; a = 0.5; p0 = 26.19; e0 = 0.8; x_I0 = -1.0;
+# m1 = 1e5; m2 = 10; a = 0.5; p0 = 26.19; e0 = 0.8; xI0 = -1.0;
 # SNR_choice = 30.0;
+# dist = 1.081;
 # qS = 0.8 ; phiS = 2.2; qK = 1.6; phiK = 1.2; 
 # Phi_phi0 = 2.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
-# delta_t = 2.0; T = 2.0;
+# dt = 2.0; T = 2.0;
 # Now check out eccentricity
 
-# M = 1e6; mu = 25; a = 0.998; p0 = 10.628; e0 = 0.1; x_I0 = 1.0
+# m1 = 1e6; m2 = 25; a = 0.998; p0 = 10.628; e0 = 0.1; xI0 = 1.0
 # dist = 1.0; SNR_choice = 50.0; 
 # qS = 0.5 ; phiS = 1.2; qK = 0.8; phiK = 0.2; 
 # Phi_phi0 = 1.0; Phi_theta0 = 0.0; Phi_r0 = 3.0
 
-# delta_t = 5.0; T = 2.0;
+# dt = 5.0; T = 2.0;

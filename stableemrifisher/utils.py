3a6da85bcd8257a485b107749eb857367a438428
Ollie Burke <ollie.burke@glasgow.ac.uk>
2025-08-28 13:00:11 +0100

    removed noise_PSD_AE function + added better plunge check

--- stableemrifisher/utils.py
+++ stableemrifisher/utils.py
@@ -445,20 +445,16 @@ def check_if_plunging(traj, T, m1, m2,a,p0,e0,xI0,Phi_phi0, Phi_theta0, Phi_r0, 
     """
     ONE_HOUR = 60*60
 
-    t_traj, p_traj, e_traj, xI_traj, Phi_phi_traj, Phi_r_traj, Phi_theta_traj = traj(m1, m2, a, 
-                                                                                    p0, e0, xI0,
-                                                                                    Phi_phi0=Phi_phi0, 
-                                                                                    Phi_theta0=Phi_theta0, 
-                                                                                    Phi_r0=Phi_r0, 
-                                                                                    T=T)
+    out = traj(m1, m2, a, 
+                    p0, e0, xI0,
+                    Phi_phi0=Phi_phi0, 
+                    Phi_theta0=Phi_theta0, 
+                    Phi_r0=Phi_r0, 
+                    T=T)
+    t_traj = out[0]
     if t_traj[-1] < T*YRSID_SI:
         print("Ah, looks like things are plunging, nightmare. redefining time T")
         end_time_seconds = (t_traj[-1]/YRSID_SI)
         T = end_time_seconds - chop_inspiral_time*(ONE_HOUR)/YRSID_SI
-        t_traj, p_traj, e_traj, xI_traj, Phi_phi_traj, Phi_r_traj, Phi_theta_traj = traj(m1, m2, a, 
-                                                                                    p0, e0, xI0,
-                                                                                    Phi_phi0=Phi_phi0, 
-                                                                                    Phi_theta0=Phi_theta0, 
-                                                                                    Phi_r0=Phi_r0, 
-                                                                                    T=T)
+    
     return T

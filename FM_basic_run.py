2200ec6d6773005307d351e5278b2dbe2957e0ca
Ollie Burke <ollie.burke@glasgow.ac.uk>
2025-08-28 13:01:48 +0100

    updates to noise_model handling

--- FM_basic_run.py
+++ FM_basic_run.py
@@ -9,8 +9,7 @@ from few.utils.geodesic import get_separatrix
 
 from stableemrifisher.fisher import StableEMRIFisher
 from stableemrifisher.utils import check_if_plunging
-from stableemrifisher.noise import write_psd_file, load_psd_from_file
-# from lisatools.sensitivity import get_sensitivity, A1TDISens, E1TDISens, T1TDISens
+from lisatools.sensitivity import get_sensitivity, A1TDISens, E1TDISens, T1TDISens
 
 from psd_utils import (write_psd_file, load_psd_from_file)
 from EMRI_Params import (m1, m2, a, p0, e0, xI0, dist, qS, phiS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0, T, dt)
@@ -113,13 +112,13 @@ if RESPONSE_FUNCTION:
     #ResponseWrapper setup
     data_channels = ['TDIA','TDIE']
     N_channels = len(data_channels)
+    print("cool")
 else:
     ResponseWrapper = None
     ResponseWrapper_kwargs = None
     data_channels = ["I", "II"]
     N_channels = len(data_channels)
 
-T = 0.1
 ####=======================True Responsed waveform==========================
 #waveform class setup
 waveform_class = FastKerrEccentricEquatorialFlux
@@ -129,10 +128,7 @@ waveform_class_kwargs = dict(inspiral_kwargs=dict(err=1e-11,),
 
 #waveform generator setup
 waveform_generator = GenerateEMRIWaveform
-if ResponseWrapper:
-    waveform_generator_kwargs = dict(return_list=False)
-else:
-    waveform_generator_kwargs = dict(return_list=True)
+waveform_generator_kwargs = dict(return_list=False, frame = "detector")
 
 
 
@@ -143,30 +139,42 @@ PSD_filename = "tdi2_AE_w_background.npy"
 
 kwargs_PSD = {"stochastic_params": [T*YRSID_SI]} # We include the background
 
-write_PSD = write_psd_file(model='scirdv1', channels='AE', 
-                           tdi2=True, include_foreground=True, 
-                           filename = run_direc + PSD_filename, **kwargs_PSD)
+write_psd_file(model='scirdv1', channels='AE', 
+                tdi2=True, include_foreground=True, 
+                filename = run_direc + PSD_filename, **kwargs_PSD)
 
-PSD_AE_interp = load_psd_from_file(run_direc + PSD_filename, xp=cp)
+PSD_AE_interp = load_psd_from_file(run_direc + PSD_filename, xp=xp)
 
 # channels = [A1TDISens, E1TDISens]
 # noise_model = get_sensitivity
 # noise_kwargs = [{"sens_fn": channel_i} for channel_i in channels]
 
-noise_model = PSD_AE_interp
-noise_kwargs = {}
-channels = ["A", "E"]
-breakpoint()
+# noise_model = PSD_AE_interp
+# noise_kwargs = {}
+# channels = ["A", "E"]
+
+noise_model = None
+noise_kwargs = None
+channels = None
+# sef = StableEMRIFisher(waveform_class=waveform_class, 
+#                        waveform_class_kwargs=waveform_class_kwargs,
+#                        waveform_generator=waveform_generator,
+#                        waveform_generator_kwargs=waveform_generator_kwargs,
+#                        ResponseWrapper=ResponseWrapper, ResponseWrapper_kwargs=ResponseWrapper_kwargs,
+#                        noise_model=noise_model, noise_kwargs=noise_kwargs, channels=channels,
+#                       stats_for_nerds = True, use_gpu = USE_GPU,
+#                       deriv_type='stable')
+
 sef = StableEMRIFisher(waveform_class=waveform_class, 
                        waveform_class_kwargs=waveform_class_kwargs,
                        waveform_generator=waveform_generator,
                        waveform_generator_kwargs=waveform_generator_kwargs,
-                       ResponseWrapper=ResponseWrapper, ResponseWrapper_kwargs=ResponseWrapper_kwargs,
                        noise_model=noise_model, noise_kwargs=noise_kwargs, channels=channels,
-                       stats_for_nerds = True, use_gpu = USE_GPU,
-                       deriv_type='stable')
+                      stats_for_nerds = True, use_gpu = USE_GPU,
+                      deriv_type='stable')
 
-param_names = ['m1','m2','a','p0','e0','dist','qS','phiS','qK','phiK','Phi_phi0','Phi_r0']
+# param_names = ['m1','m2','a','p0','e0','dist','qS','phiS','qK','phiK','Phi_phi0','Phi_r0']
+param_names = ['m1','m2','a','p0','e0','Phi_phi0','Phi_r0']
 der_order = 4
 Ndelta = 8
 stability_plot = True
@@ -210,70 +218,3 @@ param_cov = np.linalg.inv(fisher_matrix)
 
 for k, item in enumerate(param_names):
     print("Precision measurement in param {} is {}".format(item, param_cov[k,k]**(1/2)))
-
-breakpoint()
-# ================= Compute fluctuation due to noise realisation ========================
-def zero_pad(data):
-    """
-    Inputs: data stream of length N
-    Returns: zero_padded data stream of new length 2^{J} for J \in \mathbb{N}
-    """
-    N = len(data)
-    pow_2 = xp.ceil(np.log2(N))
-    return xp.pad(data,(0,int((2**pow_2)-N)),'constant')
-
-def inner_prod(sig1_f,sig2_f,N_t,delta_t,PSD):
-    """
-    Compute stationary noise-weighted inner product
-    Inputs: sig1_f and sig2_f are signals in frequency domain 
-            N_t length of padded signal in time domain
-            delta_t sampling interval
-            PSD Power spectral density
-
-    Returns: Noise weighted inner product 
-    """
-    prefac = 4*delta_t / N_t
-    sig2_f_conj = xp.conjugate(sig2_f)
-    return prefac * xp.real(xp.sum((sig1_f * sig2_f_conj)/PSD))
-
-
-N_params = len(param_names)
-derivs_A_channel = [zero_pad(derivs[p][0]) for p in range(N_params)]
-derivs_E_channel = [zero_pad(derivs[p][1]) for p in range(N_params)]
-
-derivs_A_fft = [xp.fft.rfft(derivs_A_channel[p]) for p in range(N_params)]
-derivs_E_fft = [xp.fft.rfft(derivs_E_channel[p]) for p in range(N_params)]
-
-N_t = len(derivs_A_channel[0])
-
-freq_bins = np.fft.rfftfreq(N_t, dt)
-freq_bins[0] = freq_bins[1]
-
-PSD_AE = PSD_AE_interp(freq_bins)
-
-variance_noise_AE = [N_t * PSD_AE[k] / (4*dt) for k in range(N_channels)]
-
-variance_noise_AE[0] = 2*variance_noise_AE[0]
-variance_noise_AE[-1] = 2*variance_noise_AE[-1]
-
-np.random.seed(1)
-noise_f_AE_real = [xp.random.normal(0,np.sqrt(variance_noise_AE[k])) for k in range(N_channels)]
-noise_f_AE_imag = [xp.random.normal(0,np.sqrt(variance_noise_AE[k])) for k in range(N_channels)]
-
-# Compute noise in frequency domain
-noise_f_AE = xp.asarray([noise_f_AE_real[k] + 1j * noise_f_AE_imag[k] for k in range(N_channels)])
-
-for i in range(N_channels):
-    noise_f_AE[i][0] = noise_f_AE[i][0].real
-    noise_f_AE[i][-1] = noise_f_AE[i][-1].real
-
-noise_f_A = noise_f_AE[0]
-noise_f_E = noise_f_AE[1]
-
-bias_vec_1 = np.array([inner_prod(derivs_A_fft[k],noise_f_A,N_t,dt,PSD_AE[0]) for p in range(N_params)])
-bias_vec_1 = np.array([inner_prod(derivs_A_fft[k],noise_f_A,N_t,dt,PSD_AE[0]) for p in range(N_params)])
-
-
-breakpoint()
-
-
